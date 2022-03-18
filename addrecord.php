c2d0d7894448f33f78d719a97ebe0e0bd4db5426
mattjaqueca <98959782+mattjaqueca@users.noreply.github.com>
2022-03-18 21:23:24 +0800

    Add files via upload

--- addrecord.php
+++ addrecord.php
@@ -12,8 +12,6 @@
             $('#class').val('Grade 10');
           }else if($('#yr').val() == '4'){
             $('#class').val('Grade 11');
-          }else if($('#yr').val() == '5'){
-            $('#class').val('Grade 12');
           };
     
 });
